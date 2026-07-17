b55bfd3ef7297d47131c0d6a90a9147931ae89b7
Gwennael Jean <gwennael.jean@linkt.fr>
2026-07-17 08:00:19 +0200

    feature(components): ajoute les alias pour les imports

--- src/app/layout/RootLayout.tsx
+++ src/app/layout/RootLayout.tsx
@@ -1,5 +1,5 @@
 import {Outlet} from "react-router-dom";
-import Topbar from "../../shared/ui/Navbar/Topbar.tsx";
+import Topbar from "@shared/ui/Navbar/Topbar";
 
 export default function RootLayout() {
     return (
